3e079fa3353df00f7697b134ce58b99351c73fd6
Shreyas Dixit <shreyasrd31@gmail.com>
2022-02-03 12:51:45 +0530

    first commit

---
+++ API/main.py
@@ -0,0 +1,33 @@
+from fastapi import FastAPI, File, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
+import numpy as np
+from io import BytesIO
+from PIL import Image       #Lib for Image reading
+import tensorflow as tf
+
+app = FastAPI()
+
+MODEL=tf.keras.models.load_model("Saved_Models/1")
+CLASS_NAMES=["Early Blight","Late Bilght","Healthy"]
+#-------------------------------------Functions---------------------------------#
+def read_file_as_image(data) -> np.ndarray:
+    image = np.array(Image.open(BytesIO(data)))
+    return image
+#-------------------------------------Request---------------------------------#
+@app.post("/predict")
+async def predict(
+    file: UploadFile = File(...)
+):
+    image = read_file_as_image(await file.read())
+    img_batch = np.expand_dims(image, 0)
+    
+    predictions = MODEL.predict(img_batch)
+    predicted_class=CLASS_NAMES[np.argmax(predictions[0])]
+    confidence=np.max(predictions[0])
+    return {'class':(predicted_class),'confidence':{float(confidence)}}
+    pass
+
+    
+if __name__== "__main__":
+    uvicorn.run(app,host='localhost', port=8080)
