6da4928ff5e85a30e5c6bce75ef6af4d17f1472f
Shreyas Dixit <shreyasrd31@gmail.com>
2022-02-06 20:33:24 +0530

    Commit 2

--- API/main.py
+++ API/main.py
@@ -24,7 +24,8 @@ async def predict(file: UploadFile = File(...)):
     predictions = MODEL.predict(img_batch)
     predicted_class=CLASS_NAMES[np.argmax(predictions[0])]
     confidence=np.max(predictions[0])
-    return {'class':(predicted_class),'confidence':{float(confidence)}}
+    return {'class':(predicted_class),'confidence':{float(confidence)}
+    }
     pass
 
     
